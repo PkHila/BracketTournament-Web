bfe3cad8ccfcd3370bf6c6d97e5d3bc467ef7f32
Hilario Monti <105527173+PkHila@users.noreply.github.com>
2023-11-03 11:56:54 -0300

    Revert "Vista y componente derecho agregados a CreatePoolFrame"
    This reverts commit bcf2e54d4af0c037b3b6bca41c8bba98cfa90916.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CreateContestantPoolFrameComponent } from './components/createContestantPoolFrame/create-contestant-pool-frame/create-contestant-pool-frame.component';
-import { SelectedContestantsListComponent } from './components/createContestantPoolFrame/selected-contestants-list/selected-contestants-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CreateContestantPoolFrameComponent,
-    SelectedContestantsListComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
