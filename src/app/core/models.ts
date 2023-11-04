a344c61f870d41796544902366d2c199b78823a0
NDondero <118403082+NDondero@users.noreply.github.com>
2023-11-04 18:53:50 -0300

    progreso en servicio omdb
    se agrega una clase de prueba por si hay que instanciar, pero tal vez con el mapeo a la interfaz sea suficiente

---
+++ src/app/core/models.ts
@@ -0,0 +1,14 @@
+export class TestContestant {
+
+    name: string;
+    img: string | null;
+    date: string | null;
+    author: string | null;
+
+    constructor() {
+        this.name = '';
+        this.img = '';
+        this.date = '';
+        this.author = '';
+    }
+}
--- src/app/modules/services/omdb-api.service.ts
+++ src/app/modules/services/omdb-api.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { TestContestant } from 'src/app/core/models';
 
 @Injectable({
   providedIn: 'root'
@@ -16,7 +17,34 @@ export class OMDbApiService {
     console.log(err);
 }) */
 
-  private baseURL: string = "https://www.omdbapi.com";
+  private baseUrl: string = "https://www.omdbapi.com";
+  private seriesUrl: string = "type=series";
+  private moviesUrl: string = "type=movie";
+  private notAnEnvironmentApiKey: string = "I-C-U";
 
   constructor() { }
+
+  public async getSeries(query: string): Promise<TestContestant[]> { // todo: agregar paginacion para mas de 10 resultados
+
+    return new Promise<TestContestant[]>((resolve, reject) => {
+      const contestants = new Array<TestContestant>();
+
+      fetch(`${this.baseUrl}/?s=${query}&apikey=${this.notAnEnvironmentApiKey}&${this.seriesUrl}`)
+        .then(response => {
+          if(response.ok) {
+            response.json().then(
+              // mappear la respuesta a un arreglo de contestants 
+            )
+          }
+        })
+        .catch(err => {
+          console.log(err);        
+        })
+    })
+    
+  }
+
+  public getMovies(query: string) {
+
+  }
 }
