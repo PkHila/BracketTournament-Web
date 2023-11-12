927571f2b274a39687c7a704fe86c20fd1ecfee3
NDondero <118403082+NDondero@users.noreply.github.com>
2023-11-12 09:29:03 -0300

    barra de busqueda creada
    comienza el trabajo sobre la search bar

---
+++ src/app/shared/components/search-bar/search-bar.component.ts
@@ -0,0 +1,23 @@
+import { ChangeDetectionStrategy, Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SearchBarComponent {
+  public searchTerm: string = ''
+  @Output() search = new EventEmitter<string>;
+
+  public onSearch () {
+    this.search.emit(this.searchTerm);
+  }
+
+  /**
+   * todo:
+   * reactive Form para prevenir vacios
+   * tal vez mostrar las busquedas recientes 
+   * aunque eso no esta implementado para prevenir pegarle a la api
+   */
+}
