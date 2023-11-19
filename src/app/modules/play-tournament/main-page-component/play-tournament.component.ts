018eca54a154f5abbce4d21f9538bac0a413a30a
PkHila <105527173+PkHila@users.noreply.github.com>
2023-11-19 19:36:12 -0300

    Navbar y componentes contestant1 y contestant2

--- src/app/modules/play-tournament/main-page-component/play-tournament.component.ts
+++ src/app/modules/play-tournament/main-page-component/play-tournament.component.ts
@@ -1,16 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ContestantCardBigComponent } from '../components/contestant-card-big/contestant-card-big.component';
+import { SharedModule } from "../../../shared/shared.module";
+
 
 @Component({
-  selector: 'app-play-tournament',
-  standalone: true,
-  imports: [
-    CommonModule,
-    ContestantCardBigComponent
-  ],
-  templateUrl: './play-tournament.component.html',
-  styleUrls: ['./play-tournament.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'app-play-tournament',
+    standalone: true,
+    templateUrl: './play-tournament.component.html',
+    styleUrls: ['./play-tournament.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [
+        CommonModule,
+        ContestantCardBigComponent,
+        SharedModule
+    ]
 })
 export class PlayTournamentComponent { }
--- src/app/modules/play-tournament/play-tournament.module.ts
+++ src/app/modules/play-tournament/play-tournament.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { PlayTournamentRoutingModule } from './play-tournament-routing.module';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+  ],
   imports: [
     CommonModule,
-    PlayTournamentRoutingModule
+    PlayTournamentRoutingModule,
+    SharedModule
   ]
 })
 export class PlayTournamentModule { }
