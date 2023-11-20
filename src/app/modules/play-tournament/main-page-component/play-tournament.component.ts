cc63f7b925b5e05236ccd8588e4995f3631a285d
PkHila <105527173+PkHila@users.noreply.github.com>
2023-11-20 16:54:43 -0300

    Refactor de winner component a elemento html
    Se decidió que no era necesario tener un componente separado para el elemento que muestra el ganador y se convirtió en un elemento HTML dentro de la vista

--- src/app/modules/play-tournament/main-page-component/play-tournament.component.ts
+++ src/app/modules/play-tournament/main-page-component/play-tournament.component.ts
@@ -1,11 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, Input, OnChanges, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnChanges, OnInit} from '@angular/core';
 import { ContestantCardBigComponent } from '../components/contestant-card-big/contestant-card-big.component';
 import { SharedModule } from "../../../shared/shared.module";
 import { MatchupTrackerComponent } from "../components/matchup-tracker/matchup-tracker.component";
 import { TournamentProgressComponent } from "../components/tournament-progress/tournament-progress.component";
-import { WinnerCardComponent } from '../components/winner-card/winner-card.component'; 
-import { Contestant, Round, Tournament } from 'src/app/core/interfaces';
+import { Contestant, Tournament } from 'src/app/core/interfaces';
 import { TournamentService } from 'src/app/core/services/Tournament.service';
 @Component({
     selector: 'app-play-tournament',
@@ -18,8 +17,7 @@ import { TournamentService } from 'src/app/core/services/Tournament.service';
         ContestantCardBigComponent,
         SharedModule,
         MatchupTrackerComponent,
-        TournamentProgressComponent,
-        WinnerCardComponent
+        TournamentProgressComponent
     ]
 })
 export class PlayTournamentComponent implements OnInit {
