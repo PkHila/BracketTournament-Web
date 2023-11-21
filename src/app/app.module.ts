7369c1e8fb426faba5b9668c3d7faf506d48c05c
PkHila <105527173+PkHila@users.noreply.github.com>
2023-11-21 01:15:48 -0300

    Routing de navbar a create-element

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Routes } from '@angular/router';
+import { CreateTemplateModule } from './modules/create-template/create-template.module';
+
+export const routes: Routes = [
+  { path: 'create-template', title: 'Crea un nuevo torneo', component: CreateTemplateModule }
+];
 
 @NgModule({
   declarations: [
