3692abec5053787373805fc12ecf9d3121d21efd
PkHila <105527173+PkHila@users.noreply.github.com>
2023-11-21 03:09:42 -0300

    Modulo Homepage
    Se creó pero todavía no está asociado a un path.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './modules/page-not-found/page-not-found.component';
 
 const routes: Routes = [
+  { path: '', loadChildren: () => import("./modules/home-page/home-page.module").then(m => m.HomePageModule) },
   {
     path: 'create-template', title: 'Crea un nuevo torneo',
     loadChildren: () => import("./modules/create-template/create-template.module").then(m => m.CreateTemplateModule)
--- src/app/modules/home-page/home-page.module.ts
+++ src/app/modules/home-page/home-page.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomePageRoutingModule } from './home-page-routing.module';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LandingPageComponent
+  ],
   imports: [
     CommonModule,
     HomePageRoutingModule
