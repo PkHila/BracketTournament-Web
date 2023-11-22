7b83f9ce4eaeb0409c4588d1592a5fa2c5b81bb4
NDondero <118403082+NDondero@users.noreply.github.com>
2023-11-22 18:02:11 -0300

    agregada rawg api para juegos

---
+++ src/app/modules/create-template/services/rawg-io-api.service.ts
@@ -0,0 +1,47 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { Contestant } from 'src/app/core/interfaces';
+import { environment } from 'src/environments/environment.development';
+import { ApiService, QueryParams, RawgIoResponse, RawgIoResult } from './types/interfaces';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RawgIoApiService implements ApiService {
+
+  private baseUrl = "https://api.rawg.io/api/games";
+
+  constructor(private http: HttpClient) { }
+
+  public getContestants(queryParams: QueryParams): Observable<Contestant[]> {
+    const searchUrl = `${this.baseUrl}?key=${environment.rawgApikey}&search=${queryParams.query}`;
+    return this.http.get<RawgIoResponse>(searchUrl)
+      .pipe<RawgIoResult[]>(
+        map(response => {
+          if (response.count === 0) {
+            throw new Error('No se encontraron resultados');
+          }
+          return response.results;
+        })
+      )
+      .pipe<Contestant[]>(
+        map(results => {
+          const contestants = results.map<Contestant>(result => {
+            const contestant: Contestant = {
+              name: result.name,
+              img: result.background_image,
+              author: ''
+            }
+            if (result.tba === true) {
+              contestant.date = 'tba';
+            } else {
+              contestant.date = result.released;
+            }
+            return contestant
+          })
+          return contestants;
+        })
+      )
+  }
+}
--- src/app/modules/create-template/services/types/interfaces.ts
+++ src/app/modules/create-template/services/types/interfaces.ts
@@ -1,3 +1,15 @@
+import { Observable } from "rxjs";
+import { Contestant } from "src/app/core/interfaces";
+
+export interface ApiService {
+    getContestants(queryParams: QueryParams): Observable<Contestant[]>;
+}
+
+export interface QueryParams {
+    query?: string;
+    category?: string;
+}
+
 export interface OMDbResponse {
     Response: string;
     totalResults: string | number;
@@ -10,5 +22,50 @@ export interface OMDbSearchResult {
     imdbID: string;
     Type: string;
     Poster: string;
+}
 
+export interface RawgIoResponse {
+    count: number;
+    next: string;
+    previous: string;
+    results: RawgIoResult[];
+}
+
+export interface RawgIoResult {
+    id: number;
+    slug: string;
+    name: string;
+    released: string;
+    tba: boolean;
+    background_image: string;
+    rating: number;
+    rating_top: number;
+    ratings: object;
+    ratings_count: number;
+    reviews_text_count: string;
+    added: number;
+    added_by_status: object;
+    metacritic: number;
+    playtime: number;
+    suggestions_count: number;
+    updated: string;
+    esrb_rating: {
+        id: number;
+        slug: string;
+        name: string;
+    },
+    platforms: [
+        {
+            platform: {
+                id: number;
+                slug: string;
+                name: string;
+            },
+            released_at: string,
+            requirements: {
+                minimum: string;
+                recommended: string;
+            }
+        }
+    ]
 }
