62bea21bd786c80810ed0341eb1458d7d98658e6
NDondero <118403082+NDondero@users.noreply.github.com>
2023-11-22 18:54:49 -0300

    visualizacion de una plantilla
    en la inicializacion del componente se busca el torneo por el nombre que llega por parametro en la ruta

--- src/app/modules/view-template/components/view-template-page/view-template-page.component.ts
+++ src/app/modules/view-template/components/view-template-page/view-template-page.component.ts
@@ -1,6 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Contestant, Template } from 'src/app/core/interfaces';
 import { ContestantMinimalComponent } from '../contestant-minimal/contestant-minimal.component';
+import { ActivatedRoute } from '@angular/router';
+import { TemplateService } from 'src/app/core/services/Template.service';
 
 @Component({
   selector: 'app-view-template-page',
@@ -8,14 +10,37 @@ import { ContestantMinimalComponent } from '../contestant-minimal/contestant-min
   styleUrls: ['./view-template-page.component.scss']
 })
 
-export class ViewTemplatePageComponent {
+export class ViewTemplatePageComponent implements OnInit {
   @Input() template!: Template;
+
+  constructor(
+    private route: ActivatedRoute,
+    private templatetService: TemplateService) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe({
+      next: params => {
+        const templateName = params.get('templateName');
+        this.templatetService.getTemplateByName(templateName!).subscribe({
+          next: t => {
+            this.template = t;
+          },
+          error: err => {
+            console.log(err);
+          }
+        })
+      },
+      error: err => {
+        console.log(err);
+      }
+    })
+  }
 
   public testContestant: Contestant = {
     name: "Test Contestant",
-    tournamentsPlayed:20,
-    tournamentsWon:2,
-    matchesPlayed:15,
-    matchesWon:6
+    tournamentsPlayed: 20,
+    tournamentsWon: 2,
+    matchesPlayed: 15,
+    matchesWon: 6
   };
 }
