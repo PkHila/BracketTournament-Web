a5d837954fa2a41eb686275573c6ada0ee762fdc
NDondero <118403082+NDondero@users.noreply.github.com>
2024-02-20 09:12:06 -0300

    last fm integrado
    falta una imagen para la seleccion de categorias
    el dropdown del navbar no llega a mostrar la nueva categoria, el link esta, es clickeable, pero no visible

--- src/app/modules/create-template/services/last-fm-api.service.ts
+++ src/app/modules/create-template/services/last-fm-api.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
-import { ApiService, QueryParams } from './types/interfaces';
-import { Observable } from 'rxjs';
+import { ApiService, LastFMResponse, QueryParams } from './types/interfaces';
+import { Observable, map } from 'rxjs';
 import { Contestant } from 'src/app/core/interfaces';
+import { environment } from 'src/environments/environment.development';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -10,15 +12,36 @@ export class LastFMApiService implements ApiService {
 
   private baseUrl = "http://ws.audioscrobbler.com/2.0";
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getContestants(queryParams: QueryParams): Observable<Contestant[]> {
-    throw new Error('Method not implemented.');
-    const searchUrl = `${this.baseUrl}/?method=album.search&album=${queryParams.query}&api_key=${environment.lastFMApiKey}&format=json&limit=20`;
+    
+    const searchUrl = `${this.baseUrl}/?method=album.search&album=${queryParams.query}&api_key=${environment.lastfmApiKey}&format=json&limit=20`;
+
+    return this.http.get<LastFMResponse>(searchUrl)
+    .pipe<Contestant[]>(
+      map(response => {
+        if (response.results.albummatches.album.length === 0) {
+          throw new Error('No se encontraron resultados');
+        } else {
+          const contestants = response.results.albummatches.album.map<Contestant> (album => {
+            const img = album.image.at(-1)?.['#text'];
+            const contestant: Contestant = {
+              name: album.name,
+              author: album.artist,
+              date: '',
+              img: this.checkIfImgEmpty(img!),
+            }
+            return contestant;
+          })
+          return contestants;
+        }
+      })
+    )
   }
   checkIfImgEmpty(source: string): string {
     if (source) {
-      return "/assets/img_not_found.png";
+      return source;
     } else {
       return "/assets/img_not_found.png";
     }
