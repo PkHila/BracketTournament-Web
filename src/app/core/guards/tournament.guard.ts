664afcbbc185562ed96e6b3ba36d0db604ff724c
NDondero <118403082+NDondero@users.noreply.github.com>
2024-06-06 16:13:34 -0300

    tournament guard update
    el guard compara con la cantidad maxima de rondas de un torneo determinado en lugar de un numero arbitrario

--- src/app/core/guards/tournament.guard.ts
+++ src/app/core/guards/tournament.guard.ts
@@ -1,15 +1,28 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
 
+import { TemplateService } from '../services/Template.service';
+import { catchError, map, of } from 'rxjs';
+
 export const tournamentGuard: CanActivateFn = (route) => {
   const router = inject(Router);
+  const templateService = inject(TemplateService);
   const totalRoundsParam = route.paramMap.get('totalRounds');
   const templateName = route.paramMap.get('templateName')
   if (totalRoundsParam !== null) {
     const totalRounds = parseInt(totalRoundsParam);
-    if (Number.isInteger(totalRounds) && totalRounds >= 2 && totalRounds < 7) {
-      return true;
-    }
+    return templateService.getTemplateByName(templateName ?? "")
+      .pipe(
+        map(t => {
+          const maxRoundCount = templateService.calculateMaxRoundCount(t.contestants?.length ?? 0);
+          if (Number.isInteger(totalRounds) && totalRounds >= 2 && totalRounds <= maxRoundCount) {
+            return true;
+          } else {
+            return router.parseUrl(`404`);
+          }
+        }),
+        catchError(() => of(router.parseUrl(`404`)))
+      );
   }
   return router.parseUrl(`404`);
 };
